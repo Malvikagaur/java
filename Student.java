5de4cb4ad1adbe3d6206405695bc7d2491ad5d78
Malvikagaur <gaurmalvikamg@gmail.com>
2024-09-01 16:34:03 +0530

    oops concept

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Features{
+    int roll_num;
+    String name;
+}
+public class Student {
+    public static void main(String[] args) {
+        Features obj1 = new Features();
+        obj1.roll_num = 1;
+        obj1.name = "abc";
+        System.out.println(obj1.name);
+        System.out.println(obj1.roll_num);
+    }
+}
