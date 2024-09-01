baa56d6854608a329e65fe25e0f97ca034fef26b
Malvikagaur <gaurmalvikamg@gmail.com>
2024-09-01 14:09:45 +0530

    loops and pattern

---
+++ NumPyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j <= r-i; j++){ //space                                           //          1
+                System.out.print(" ");                                                     //         121
+            }                                                                                //        12321
+            for(int k = 1; k <= i; k++){  //123 vali series
+                System.out.print(k);
+            }
+            for(int l = i - 1; l >= 1; l--){ //321 vali series
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}
