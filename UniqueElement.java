415d506c6f043fb025a7fb5fcb20d98d491d6b95
Malvikagaur <malvikagaurmg@gmail.com>
2025-01-16 13:02:58 +0530

    array manipulation

--- UniqueElement.java
+++ UniqueElement.java
@@ -1,22 +1,28 @@
 
 import java.util.Scanner;
 
-public class TripletSum {
-    static int findSum(int arr [], int target){
-        int ans = 0;
+public class UniqueElement {
+    static int findUnique(int arr[]){
+        int ans = -1;
         int n = arr.length;
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n;i++){
             for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(arr[i] + arr[j] + arr[k] == target){
-                        ans++;
-                    }
+                if(arr[i] == arr[j]){
+                    arr[i] = -1;
+                    arr[j] = -1;
                 }
             }
         }
-        return ans;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] > 0){
+                ans = arr[i];
+            }
+        }
+        return  ans;
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the size of array : ");
@@ -27,9 +33,6 @@ public class TripletSum {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.print("enter the target sum : ");
-        int target = sc.nextInt();
-
-        System.out.println("the no. of pairs forming the target sum are : " +findSum(arr, target));
+        System.out.println("the unique element is : " +findUnique(arr));
     }
 }
